129d1cfaa43662abd75c7d928e578e6ecbe86d64
JesabelKern <jesabelkern@gmail.com>
2023-01-13 21:30:57 -0300

    Realicé ultimos ejercicios

--- app.js
+++ app.js
@@ -29,10 +29,11 @@ const crearTarea = (titulo, estado) =>{
     tareas.push ({Titulo: titulo, Estado: estado})
     return tareas
 }
-   console.log (crearTarea("Hacer ejercicios de Mumuki", "En proceso"))
-   console.log (tareas)
+console.log (crearTarea("Hacer ejercicios de Mumuki", "En progreso"))
+console.log (tareas)
    
 //DESAFIO 3//
+
 const filtrarTareas = (estado) => {
 for(const tarea of tareas){ // itero para que busque el estado --- mi constante tarea 
     if (tarea.Estado ===  estado) {  // Si el estado de todos los parametros es igual a estado 
@@ -46,4 +47,32 @@ for(const tarea of tareas){ // itero para que busque el estado --- mi constante 
    console.log(filtrarTareas("En progreso"))
    
   
-//
+//DESAFIO 4//
+
+const agregarTarea = (titulo, estado) =>{
+    for (let index = 0; index < tareas.length; index++) {
+        if(tareas[index].Titulo === titulo) {
+            return "Error, ya existe esa tarea"
+        }
+    }
+} 
+ console.log(agregarTarea("Hacer ejercicios de Mumuki", "Pendiente"))
+
+//DESAFIO 5//
+/*Existe tarea?
+
+Crear una funcion que devolvera las tareas en donde el titulo coincida con el parametro dado. 
+Ejemplo: existeTarea(“js”) // Devera devolver el array con los objetos que coincidan con el string sin importar su estado
+*/
+const existeTarea = (str) => {
+const tareasFiltradas = []
+tareas. forEach(tareita => {
+    if(tareita.Titulo.includes(str)) {
+        tareasFiltradas.push(tareita)
+    }
+})
+return tareasFiltradas
+}
+
+console.log(existeTarea("js"))
+  
